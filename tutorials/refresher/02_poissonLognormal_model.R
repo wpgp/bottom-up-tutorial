1d9afb605b50d0f445cd7a71490e71fd236bd225
edarin <e.c.darin@soton.ac.uk>
2022-05-05 18:17:52 +0100

    split for tutorial flow

---
+++ tutorials/refresher/02_poissonLognormal_model.R
@@ -0,0 +1,234 @@
+library(rstan)
+library(tidyverse)
+library(here)
+library(plotly)
+
+options(mc.cores = parallel::detectCores()-1)
+rstan_options(auto_write = TRUE)
+
+
+chains <- 4
+iter <- 500
+warmup <- 300
+seed <- 12345677
+
+observations <- read_csv(here('tutorials', 'refresher','observations.csv'),
+                         col_types = "nf")
+
+# Descriptive statistics --------------------------------------------------
+
+
+hist(observations$pop)
+
+
+# Fit a Poisson model -----------------------------------------------------
+
+# Prior predictive check
+alpha_national_simulated <- abs(rnorm(1000, 500, 50))
+pop_poisson_simulated <- sapply(alpha_national_simulated, function(x) rpois(1, x))
+
+hist(pop_poisson_simulated)
+
+# fit the model
+input_data <- list(
+  n_obs =  nrow(observations),
+  pop = observations$pop
+)
+
+pars_poisson <- c('alpha_national', 'pop_post_pred')
+
+fit_poisson <- stan(
+  file= here('tutorials', 'refresher','poisson.stan'),
+  data= input_data,
+  iter = iter + warmup,
+  warmup = warmup,
+  seed = seed,
+  pars = pars_poisson
+)
+
+# extract model outputs
+samples_poisson <- rstan::extract(fit_poisson)
+
+# visualise convergence
+traceplot(fit_poisson, pars = 'alpha_national')
+
+# prior retrodective check
+alpha_national <- tibble(
+  posterior = samples_poisson$alpha_national,
+  prior = abs(rnorm(chains*iter, 500, 50)),
+  iter = 1:(chains*iter)
+) %>% 
+  pivot_longer(-iter, names_to = 'distribution')
+
+ggplot(alpha_national, aes(x=value, fill=distribution))+
+  geom_histogram(binwidth = 5)+
+  theme_minimal()
+
+# posterior predictive check
+
+pop_posterior <- tibble(
+  source = factor(c(rep('predicted', iter*chains*input_data$n_obs), 
+                    rep('observed', input_data$n_obs)), 
+                  levels = c('predicted', 'observed')),
+  value= c(as.vector(samples_poisson$pop_post_pred), input_data$pop)
+)
+
+
+ggplot(pop_posterior, aes(x=value, fill=source, after_stat(density)))+
+  geom_histogram(binwidth=5, position='identity', alpha=0.7)+
+  theme_minimal()
+
+# Fit a Poisson-Lognormal model -----------------------------------------------------
+
+# Prior predictive check
+alpha_simulated <- abs(rnorm(1000, log(500), 0.1))
+sigma_simulated <- abs(rnorm(1000, 0.3, 0.1))
+lambda_simulated <- mapply( function(x,y) rlnorm(1, x, y), alpha_simulated, sigma_simulated )
+pop_lognormal_simulated <- sapply(lambda_simulated, function(x) rpois(1, x))
+hist(pop_lognormal_simulated)
+
+comp_pop <- tibble(
+  model = factor(c(rep('Poisson', length(pop_poisson_simulated)), 
+                   rep('Poisson-Lognormal',length(pop_lognormal_simulated))),
+                 levels=c('Poisson-Lognormal','Poisson')),
+  value= c(pop_poisson_simulated,pop_lognormal_simulated)
+)
+
+ggplot(comp_pop, aes(x=value, fill=model))+
+  geom_histogram(bins = 100,position='identity', alpha=0.8)+
+  theme_minimal()
+
+# run the model
+pars_lognormal <- c('alpha_national', 'sigma', 'pop_post_pred')
+
+fit_poisson_lognormal <- stan(
+  file= here('tutorials', 'refresher','poisson_lognormal.stan'),
+  data= input_data,
+  iter = iter + warmup,
+  warmup = warmup,
+  seed = seed,
+  pars = pars_lognormal
+)
+
+traceplot(fit_poisson_lognormal, pars = 'alpha_national')
+traceplot(fit_poisson_lognormal, pars = 'sigma')
+
+
+samples_poisson_lognormal <- rstan::extract(fit_poisson_lognormal)
+
+# prior retrodictive check
+alpha_national <- tibble(
+  posterior = samples_poisson_lognormal$alpha_national,
+  prior = abs(rnorm(chains*iter, log(500), 0.1)),
+  iter = 1:(chains*iter)
+) %>% 
+  pivot_longer(-iter, names_to = 'distribution')
+
+ggplot(alpha_national, aes(x=value, fill=distribution))+
+  geom_histogram(bins=100, position='identity', alpha=0.8)+
+  theme_minimal()
+
+sigma <- tibble(
+  posterior = samples_poisson_lognormal$sigma,
+  prior = abs(rnorm(chains*iter, 0.3, 0.1)),
+  iter = 1:(chains*iter)) %>% 
+  pivot_longer(-iter, names_to = 'distribution')
+
+ggplot(sigma, aes(x=value, fill=distribution))+
+  geom_histogram(bins=100, position='identity', alpha=0.8)+
+  theme_minimal()
+
+# posterior predictive check
+
+pop_posterior_lognormal <- tibble(
+  source = factor(c(rep('predicted_poisson', iter*chains*input_data$n_obs), 
+                    rep('predicted_poisson_lognormal', iter*chains*input_data$n_obs),
+                    rep('observed', input_data$n_obs)), 
+                  levels = c('observed', 'predicted_poisson_lognormal', 'predicted_poisson')),
+  value= c(
+    as.vector(samples_poisson$pop_post_pred), 
+    as.vector(samples_poisson_lognormal$pop_post_pred), 
+    input_data$pop)
+)
+
+
+ggplot(pop_posterior_lognormal, aes(x=value, fill=source, after_stat(density)))+
+  geom_histogram(bins=100, position='identity', alpha=0.7)+
+  theme_minimal()
+
+
+# Fit a random slope model  --------------------------------------------------
+
+# Descriptive statistics 
+
+ggplot(observations, aes(x=pop, fill=settlement, after_stat(density)))+
+  geom_histogram(bins=50, position='identity', alpha=0.8)+
+  theme_minimal()
+
+
+# run the model
+input_data <- list(
+  n_obs =  nrow(observations),
+  pop = observations$pop,
+  n_settlement = nlevels(observations$settlement),
+  obs_to_settlement = as.integer(observations$settlement)
+)
+
+
+pars_lognormal_independent <- c('pop_post_pred', 'alpha_settlement',  'sigma')
+
+
+fit_poisson_lognormal_independent <- stan(
+  file= here('tutorials', 'refresher','poisson_independent.stan'),
+  data= input_data,
+  iter = iter + warmup,
+  warmup = warmup,
+  seed = seed,
+  pars = pars_lognormal_independent
+)
+
+traceplot(fit_poisson_lognormal_independent, pars = 'alpha_settlement')
+traceplot(fit_poisson_lognormal_independent, pars = 'sigma')
+
+samples_poisson_lognormal_independent <- rstan::extract(fit_poisson_lognormal_independent)
+
+
+# prior retrodictive check
+alpha_settlement <- as_tibble(samples_poisson_lognormal_independent$alpha_settlement)
+colnames(alpha_settlement) <- c('posterior_alpha_settlement_1', 'posterior_alpha_settlement_2')
+
+alpha_settlement <- alpha_settlement %>% 
+  mutate (
+    prior = abs(rnorm(chains*iter, log(500), 0.1)),
+    iter = 1:(chains*iter)
+  ) %>% 
+  pivot_longer(-iter, names_to = 'distribution')
+
+ggplot(alpha_settlement, aes(x=value, fill=distribution))+
+  geom_histogram(bins=100,position='identity', alpha=0.7)+
+  theme_minimal()
+
+# posterior predictive check
+
+pop_posterior_lognormal_independent <- tibble(
+  source = factor(c(
+    rep('predicted_poisson_lognormal', iter*chains*input_data$n_obs),
+    rep('predicted_poisson_lognormal_independent', iter*chains*input_data$n_obs),
+    rep('observed', input_data$n_obs)), 
+    levels = c('observed','predicted_poisson_lognormal_independent', 
+               'predicted_poisson_lognormal')),
+  value= c(
+    as.vector(samples_poisson_lognormal$pop_post_pred), 
+    as.vector(samples_poisson_lognormal_independent$pop_post_pred),
+    input_data$pop)
+)
+
+
+ggplotly(ggplot(pop_posterior_lognormal_independent, aes(x=value, fill=source, after_stat(density)))+
+           geom_histogram(bins=50, position='identity', alpha=0.5)+
+           theme_minimal())
+
+
+# What about breaking down the variance? -----------------------------------
+
+
